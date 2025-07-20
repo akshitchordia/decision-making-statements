a4649f1d4a18ac9045a7dbc01e5816bc7a7232dc
akshitchordia <chordiaakshit3@gmail.com>
2025-07-20 21:46:54 +0530

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,36 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - EnTC - A1
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cout<<"Enter the number:"<<endl;
+  cin>>num;
+  if(num%2==0){
+      cout<<"The given number is even number."<<endl;
+  }
+  else{
+      cout<<"The given number is odd number."<<endl;
+  }
+    return 0;
+}
+
+/*
+
+output
+
+Enter the number:
+56
+The given number is even number.
+
+Enter the number:
+7
+The given number is odd number.
+
+
+*/
