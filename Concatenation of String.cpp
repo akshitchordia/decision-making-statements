9f27c21301422d86a7f6b9c5c982d60d0b793e67
akshitchordia <chordiaakshit3@gmail.com>
2025-08-03 21:20:09 +0530

    Create Concatenation of String.cpp

---
+++ Concatenation of String.cpp
@@ -0,0 +1,28 @@
+// Name - Akshit Chordia
+// PRN - 24070123008
+// Batch - EnTC - A1
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    string str2;
+    string str3;
+    
+    cout<<"Enter first string\n";
+    cin>>str1;
+    cout<<"Enter second string\n";
+    cin>>str2;
+    str3=str1+" "+str2;
+    cout<<"Concatenated string is "<<str3;
+    return 0;
+}
+
+// OUTPUT
+// Enter first string
+// akshit
+// Enter second string
+// chordia
+// Concatenated string is akshit chordia
